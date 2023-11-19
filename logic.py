f2a9d1c7e5a2d813db29ae58dd1639c3325ae3a2
awilson61 <144381426+awilson61@users.noreply.github.com>
2023-11-18 22:58:39 -0600

    Renamed the pictures

--- logic.py
+++ logic.py
@@ -1,6 +1,6 @@
 from PyQt6.QtWidgets import *
 from voting_gui import *
-
+import os
 
 class Logic(QMainWindow, Ui_MainWindow):
     def __init__(self):
@@ -10,11 +10,43 @@ class Logic(QMainWindow, Ui_MainWindow):
         self.vote_button.clicked.connect(lambda: self.vote())
         self.results_button.clicked.connect(lambda: self.results())
 
+        self.votes = {'Halloween': 0, 'Christmas': 0}
+        self.load_votes()
+
     def clear(self):
         pass
 
     def vote(self):
-        pass
+        try:
+            choice = self.user_input.text().strip().title()
+            if choice in self.votes:
+                self.votes[choice] += 1
+                self.save_votes()
+            else:
+                raise Exception
+        except:
+            print('wrong')
+
+    def save_votes(self):
+        # Save the current votes to the file
+        filename = "votes.txt"
+        try:
+            with open(filename, "w") as file:
+                for option, count in self.votes.items():
+                    file.write(f"{option}:{count}\n")
+        except Exception as e:
+            print(f"Error saving votes: {e}")
 
     def results(self):
-        pass
+        print("votes: ", self.votes)
+
+    def load_votes(self):
+        filename = "votes.txt"
+        try:
+            if os.path.exists(filename):
+                with open(filename, "r") as file:
+                    for line in file:
+                        option, count = line.strip().split(":")
+                        self.votes[option] = int(count)
+        except Exception as e:
+            print(f"Error loading votes: {e}")
